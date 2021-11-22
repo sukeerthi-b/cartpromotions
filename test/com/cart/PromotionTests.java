5751f8e3b6bcfc4d610b6cb7ae07c89165473a83
sukeerti <sukeerthi.botu@gmail.com>
2021-11-22 11:01:00 +0530

    refactor(promotion): feat(promotion): add draft testcases for all required scenarios

---
+++ test/com/cart/PromotionTests.java
@@ -0,0 +1,46 @@
+package com.cart;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class PromotionTests {
+
+    /**
+     * scenarioA
+     * 1-A
+     * 1-B
+     * 1-C
+     *
+     * result 100
+     */
+    @Test
+    public void scenarioA() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * scenarioB
+     * 5-A
+     * 5-B
+     * 1-C
+     *
+     * result 370
+     */
+    @Test
+    public void scenarioB() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * scenarioC
+     * 3-A
+     * 5-B
+     * 1-C
+     * 1-D
+     * result 280
+     */
+    @Test
+    public void scenarioC() {
+        fail("Not yet implemented");
+    }
+}
